db270120f7a5003e926f6e0812e17f7f569b5664
Marta Zazlak <marta.zazlak@hotmail.com>
2023-01-08 21:50:24 +0100

    update readme

--- src/context/AppContextWrapper.tsx
+++ src/context/AppContextWrapper.tsx
@@ -1,19 +1,12 @@
 import React, { useEffect } from 'react';
 import { DEFAULTS } from '../defaults';
-import { ModelsManager } from '../models';
-import { AppLangModel } from '../models/AppLangModel';
-import { AppRollModel } from '../models/AppRollModel';
-import { AppThemeModel } from '../models/AppThemeModel';
 import { getNewAppTheme } from '../services';
 import { useStateWithObservableWithInit } from '../tools';
-import { AppLangs, AppTheme, Models, TAppContext, TTheme, TTranslations } from '../types';
+import { AppLangs, AppTheme, TAppContext, TTheme, TTranslations } from '../types';
 import { AppContext } from './AppContext';
+import { appLangModel, appThemeModel } from './models';
 
 type Props = React.PropsWithChildren<unknown>;
-
-export const appThemeModel = ModelsManager.getSingleton<AppThemeModel>(Models.APP_THEME);
-export const appLangModel = ModelsManager.getSingleton<AppLangModel>(Models.APP_LANG);
-export const appRollModel = ModelsManager.getSingleton<AppRollModel>(Models.APP_ROLL);
 
 export const AppContextWrapper: React.FC<Props> = ({ children }) => {
     const theme = useStateWithObservableWithInit<TTheme>(appThemeModel.theme, DEFAULTS.THEME);
