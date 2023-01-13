f163cb1001c4b71e5b79ca795c1ac071cf2f8741
Marta Zazlak <marta.zazlak@hotmail.com>
2023-01-13 12:37:21 +0100

    app theme service - test

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
-export * from './toggleAppTheme.service';
+export * from './appTheme.service';
 export * from './rolls.service';
 export * from './dices.service';
 export * from './mod.service';
---
+++ src/tests/services/appTheme.test.ts
@@ -0,0 +1,22 @@
+import { getNewAppTheme } from '../../services/appTheme.service';
+import { AppTheme } from '../../types';
+
+describe('getNewAppTheme', () => {
+    test('Should get a new app theme based on the current one (should be an opposite)', async () => {
+        const appTheme = AppTheme.DARK;
+        const expectedResult = AppTheme.LIGHT;
+
+        const result = getNewAppTheme(appTheme);
+        expect(result).toEqual(expectedResult);
+    });
+
+    test('Should get a new app theme based on the current one (should be an opposite)', async () => {
+        const appTheme = AppTheme.LIGHT;
+        const expectedResult = AppTheme.DARK;
+
+        const result = getNewAppTheme(appTheme);
+        expect(result).toEqual(expectedResult);
+    });
+});
+
+export {};
