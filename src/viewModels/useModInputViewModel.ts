084eadd938faa6fb252f7d2dbb355c55afba1d87
Marta Zazlak <marta.zazlak@hotmail.com>
2023-01-23 16:46:51 +0100

    fate fields

--- src/viewModels/useModInputViewModel.ts
+++ src/viewModels/useModInputViewModel.ts
@@ -13,20 +13,19 @@ const currentValueSource = new BehaviorSubject<string>(INITIAL_MOD);
 const isCurrentValueOkSource = combineLatest([currentValueSource]).pipe(map(([value]) => testIfModIsOk(value)));
 
 export const useModInputViewModel = (activeMainTab: MainContentTab) => {
+    const rollModel = useMemo(() => getModelByMainTabKey(activeMainTab)!, [activeMainTab]);
+
     const { theme, translations } = useAppContext();
     const showInput = useStateWithObservableWithInit(showInputSource, DEFAULTS.SHOW_INPUT_MOD_ON_ENTER);
     const currentValue = useStateWithObservableWithInit(currentValueSource, INITIAL_MOD);
     const isCurrentValueOk = useStateWithObservableWithInit(isCurrentValueOkSource, true);
+    const currentConfirmedMod = useStateWithObservableWithInit<number>(rollModel.rollModSource, DEFAULTS.MOD, [
+        activeMainTab,
+    ]);
 
     useEffect(() => {
         currentValueSource.next(INITIAL_MOD);
     }, [activeMainTab]);
-
-    const rollModel = useMemo(() => getModelByMainTabKey(activeMainTab)!, [activeMainTab]);
-
-    const currentConfirmedMod = useStateWithObservableWithInit<number>(rollModel.rollModSource, DEFAULTS.MOD, [
-        activeMainTab,
-    ]);
 
     const toggleShowInput = () => {
         showInputSource.next(!showInput);
