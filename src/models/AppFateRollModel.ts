dfb3ff76817ba5fa900f38daba04fc9d86f5eee0
Marta Zazlak <marta.zazlak@hotmail.com>
2023-02-04 20:56:55 +0100

    tests

--- src/models/AppFateRollModel.ts
+++ src/models/AppFateRollModel.ts
@@ -4,9 +4,8 @@ import { DiceTypes, TFateRoll, TFateRollExtended } from '../types';
 import { AbstractRollModel } from './AbstractRollModel';
 
 export class AppFateRollModel extends AbstractRollModel<TFateRoll, TFateRollExtended> {
-    protected prepareExtendedRoll([roll, mod]: [TFateRoll | null, number | null]): TFateRollExtended | null {
-        return prepareExtendedFateRoll(roll, mod);
-    }
+    protected prepareExtendedRoll = ([roll, mod]: [TFateRoll | null, number | null]) =>
+        prepareExtendedFateRoll(roll, mod);
 
     protected prepareRollResult = () => {
         const rolls = rollFateDices();
@@ -18,7 +17,5 @@ export class AppFateRollModel extends AbstractRollModel<TFateRoll, TFateRollExte
         };
     };
 
-    public rollDice(): void {
-        super.rollDice(DiceTypes.FATE);
-    }
+    public rollDice = () => super.rollDice(DiceTypes.FATE);
 }
