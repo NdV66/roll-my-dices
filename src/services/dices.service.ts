01535d2201c31d743e58dcb11d61caba5f6dc104
Marta Zazlak <marta.zazlak@hotmail.com>
2023-02-11 10:59:37 +0100

    ref

--- src/services/dices.service.ts
+++ src/services/dices.service.ts
@@ -9,7 +9,7 @@ import { DiceTypes } from '../types';
 
 const _getMaxByDiceType = (diceType: DiceTypes) => DICE_TYPES_MAX.get(diceType)!;
 
-export const _mapRollToDice = (diceType: DiceTypes, rawResult: number): string => {
+const _mapRollToDice = (diceType: DiceTypes, rawResult: number): string => {
     const diceSet = ROLLS_RESULTS_FONTS[diceType];
     const index = rawResult - 1;
 
@@ -20,20 +20,15 @@ export const _mapRollToDice = (diceType: DiceTypes, rawResult: number): string =
     return diceSet[index];
 };
 
-export const mapMaxValueToDice = (diceType: DiceTypes): string => {
-    const maxResult = _getMaxByDiceType(diceType);
-
-    if (diceType === DiceTypes.FATE) {
-        throw USE_FATE_INSTEAD_ERROR;
-    }
-
-    return SPECIAL_MAX_DICE_VALUE.get(diceType) || _mapRollToDice(diceType, maxResult);
-};
-
 export const mapRollToDice = (diceType: DiceTypes, rawResult: number): string => {
     if (diceType === DiceTypes.FATE) {
         throw USE_FATE_INSTEAD_ERROR;
     }
 
     return _mapRollToDice(diceType, rawResult);
+};
+
+export const mapMaxValueToDice = (diceType: DiceTypes): string => {
+    const maxResult = _getMaxByDiceType(diceType);
+    return SPECIAL_MAX_DICE_VALUE.get(diceType) || mapRollToDice(diceType, maxResult);
 };
--- src/tests/services/dices.test.ts
+++ src/tests/services/dices.test.ts
@@ -1,28 +1,15 @@
-import { ROLLS_RESULTS_FONTS, NO_DICE_FOUND_ERROR, USE_FATE_INSTEAD_ERROR } from '../../defaults';
-import { mapRollToDice } from '../../services/dices.service';
+import {
+    ROLLS_RESULTS_FONTS,
+    NO_DICE_FOUND_ERROR,
+    USE_FATE_INSTEAD_ERROR,
+    DICE_TYPES_MAX,
+    SPECIAL_MAX_DICE_VALUE,
+} from '../../defaults';
+import { mapMaxValueToDice, mapRollToDice } from '../../services/dices.service';
+import * as diceTools from '../../services/dices.service';
 import { DiceTypes } from '../../types';
 
 describe('mapRollToDice', () => {
-    test('Should select a right sign', () => {
-        const diceType = DiceTypes.D_20;
-        const roll = 6;
-        const expectedResult = ROLLS_RESULTS_FONTS[diceType][roll - 1];
-
-        const result = mapRollToDice(diceType, roll);
-
-        expect(result).toEqual(expectedResult);
-    });
-
-    test('Should select a right sign (max value)', () => {
-        const diceType = DiceTypes.D_20;
-        const roll = 20;
-        const expectedResult = ROLLS_RESULTS_FONTS[diceType][roll - 1];
-
-        const result = mapRollToDice(diceType, roll);
-
-        expect(result).toEqual(expectedResult);
-    });
-
     test('Should throw NO_DICE_FOUND, when the roll number is not supported', () => {
         const callback = () => {
             mapRollToDice(DiceTypes.D_20, 126);
@@ -38,16 +25,51 @@ describe('mapRollToDice', () => {
 
         expect(callback).toThrow(USE_FATE_INSTEAD_ERROR);
     });
+
+    describe('Should select a right sign', () => {
+        const diceType = DiceTypes.D_20;
+
+        const testShouldSelectRightSign = (roll: number) => {
+            const expectedResult = ROLLS_RESULTS_FONTS[diceType][roll - 1];
+            const result = mapRollToDice(diceType, roll);
+
+            expect(result).toEqual(expectedResult);
+        };
+
+        test('- for min value', () => {
+            const roll = 1;
+            testShouldSelectRightSign(roll);
+        });
+
+        test('- for ok value', () => {
+            const roll = 6;
+            testShouldSelectRightSign(roll);
+        });
+
+        test('- for max value', () => {
+            const roll = DICE_TYPES_MAX.get(diceType)!;
+            testShouldSelectRightSign(roll);
+        });
+    });
 });
 
-// describe('getMaxByDiceType', () => {
-//     test('Should get max by dice type', () => {
-//         const dice = DiceTypes.D_20;
-//         const max = 20;
+describe('mapMaxValueToDice', () => {
+    test('Should get max by dice type', () => {
+        const diceType = DiceTypes.D_20;
+        const roll = DICE_TYPES_MAX.get(diceType)!;
+        const expectedResult = ROLLS_RESULTS_FONTS[diceType][roll - 1];
 
-//         const result = getMaxByDiceType(dice);
-//         expect(result).toEqual(max);
-//     });
-// });
+        const result = mapMaxValueToDice(diceType);
+        expect(result).toEqual(expectedResult);
+    });
+
+    test('Should get max by dice type when dice is special', () => {
+        const diceType = DiceTypes.D_10;
+        const expectedSign = SPECIAL_MAX_DICE_VALUE.get(diceType)!;
+
+        const result = mapMaxValueToDice(diceType);
+        expect(result).toEqual(expectedSign);
+    });
+});
 
 export {};
