3bbfd7ea97d23d753fcd8eaa796a4c404bd5929a
Marta Zazlak <marta.zazlak@hotmail.com>
2023-02-15 23:26:40 +0100

    tests

---
+++ src/tests/view/RollsHeader.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { TEXTS_EN } from '../../langs/en';
+import { DARK_THEME } from '../../styles';
+import { DiceTypes } from '../../types';
+import { RollsHeader } from '../../view/elements/RollsElement';
+
+const displayValue = 'A';
+
+const DEFAULT_PROPS = {
+    theme: DARK_THEME,
+    rollsElementData: [
+        {
+            diceType: DiceTypes.D_20,
+            roll: jest.fn(),
+            displayValue,
+        },
+    ],
+    translations: TEXTS_EN,
+};
+
+describe('RollHeader', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('Should render correctly', () => {
+        const { getByText } = render(<RollsHeader {...DEFAULT_PROPS} />);
+
+        expect(getByText(DEFAULT_PROPS.translations.INFO)).toBeInTheDocument();
+        expect(getByText(displayValue)).toBeInTheDocument();
+    });
+});
+
+export {};
